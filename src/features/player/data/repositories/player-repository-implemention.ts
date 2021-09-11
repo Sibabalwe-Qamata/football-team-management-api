eeb3a743ecb56cd1ad21e2dcd6afb6230aa07ae8
sibabalwe-qamata <prof.qamata@gmail.com>
2021-09-11 23:41:17 +0200

    feat(player-endpoint): added player repository implementation

--- src/features/player/data/repositories/player-repository-implemention.ts
+++ src/features/player/data/repositories/player-repository-implemention.ts
@@ -1,5 +1,52 @@
-//  import { Player } from "../../domain/entities/player-entity";
-//  import { PlayerRepository } from "../../domain/repositories/player-repository";
-//  import { createPlayer } from "../datasources/create-player";
+ import { Player } from "../../domain/entities/player-entity";
+ import { PlayerRepository } from "../../domain/repositories/player-repository";
+ import { createPlayer } from "../datasources/create-player";
 
 
+export class PlayerRepositoryImplementation implements PlayerRepository{
+
+    async CreatePlayer(player:any):Promise<Player>{
+        try {
+            
+
+            return new Player("")
+        } catch (error) {
+            throw error
+        }
+    }
+
+
+    async CreatePlayers(players:any[]):Promise<Player>{
+        try {
+            return new Player("")
+        } catch (error) {
+            throw error
+        }
+    }
+
+    async EditPlayerTeam(player: any):Promise<Player>{
+        try {
+            return new Player("") 
+        } catch (error) {
+            throw error
+        }
+    }
+
+
+    async EditPlayer(player : any):Promise<Player>{
+        try {
+            return new Player("")
+        } catch (error) {
+            throw error
+        }
+    }
+
+    async GetPlayer(player:any):Promise<Player>{
+        try {
+            return new Player("")
+        } catch (error) {
+            throw error
+        }
+    }
+
+}
