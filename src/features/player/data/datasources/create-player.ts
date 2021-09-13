5e6f3d70a8eab65b652a1b477ec358fab63e0e11
sibabalwe-qamata <prof.qamata@gmail.com>
2021-09-13 21:05:52 +0200

    feat(createPlayer): updated player datasources

--- src/features/player/data/datasources/create-player.ts
+++ src/features/player/data/datasources/create-player.ts
@@ -1,15 +1,6 @@
-// import { assert, object, string, number} from 'superstruct';
+import { assert, number} from 'superstruct';
 
 import  randomstring  from 'randomstring';
-
-
-// const _player = object({
-//     team_id:string(),
-//         name: string(),
-//         surname: string(),
-//         height: number(),
-//         position:number()
-// })
 
 /**
  * Create a player 
@@ -21,15 +12,11 @@ import  randomstring  from 'randomstring';
 
  export const createPlayer = async(team_id:number, player:any): Promise<any> =>{
     try {
-    // assert(
-    // _player,
-    // player
-    // );
+    assert(team_id, number());
     
     const playerRef = randomstring.generate();
     // To-do : DB insertion 
     
-
     return playerRef
     } catch (error) {
         throw error;
