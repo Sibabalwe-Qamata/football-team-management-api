73357a2cafcd11a7189a2c5808457f18ab744968
sibabalwe-qamata <prof.qamata@gmail.com>
2021-09-13 21:42:29 +0200

    feat(team-controller): added team controller

---
+++ src/features/team/adapters/controllers/team-controller.ts
@@ -0,0 +1,44 @@
+
+
+/**
+ * Create team controller
+ * @function
+ * @param {express.Request} req
+ * @param {express.Response} res
+ * @param  {express.NextFunction} next
+ */
+
+ export const createTeamController = async(req: any, res: any, next: any) =>{
+    try {
+    
+ 
+    const result = " "
+
+    res.json(result)
+    } catch (error) {
+        next(error)
+    }
+
+};
+
+
+/**
+ * Link team to a stadium controller
+ * @function
+ * @param {express.Request} req
+ * @param {express.Response} res
+ * @param  {express.NextFunction} next
+ */
+
+ export const linkTeamToStadiumController = async(req: any, res: any, next: any) =>{
+    try {
+    
+ 
+    const result = " "
+
+    res.json(result)
+    } catch (error) {
+        next(error)
+    }
+
+};
